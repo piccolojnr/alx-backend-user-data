4d9c1846c50a345aa8dd45c25c94587b7be74c77
piccolojnr <rahimdaud246@gmail.com>
2024-05-30 23:44:45 +0000

    Refactor RedactingFormatter class to initialize with fields parameter

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -19,10 +19,12 @@ class RedactingFormatter(logging.Formatter):
     SEPARATOR = ";"
 
     def __init__(self, fields):
+        """Initializes the RedactingFormatter"""
         super(RedactingFormatter, self).__init__(self.FORMAT)
         self.fields = fields
 
     def format(self, record: logging.LogRecord) -> str:
+        """Filters values from the log"""
         og = super(RedactingFormatter, self).format(record)
         return filter_datum(self.fields, self.REDACTION, og, self.SEPARATOR)
 
