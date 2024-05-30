f642f11693d8fc9df993a23111f6223638e0097b
piccolojnr <rahimdaud246@gmail.com>
2024-05-30 23:46:34 +0000

    Refactor RedactingFormatter class to initialize with fields parameter

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -18,7 +18,7 @@ class RedactingFormatter(logging.Formatter):
     FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
     SEPARATOR = ";"
 
-    def __init__(self, fields):
+    def __init__(self, fields: List[str]):
         """Initializes the RedactingFormatter"""
         super(RedactingFormatter, self).__init__(self.FORMAT)
         self.fields = fields
