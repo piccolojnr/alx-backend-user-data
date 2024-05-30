83626a1b3668127c07f7d5d0d8ad23988a97e070
piccolojnr <rahimdaud246@gmail.com>
2024-05-30 23:52:53 +0000

    Refactor encrypt_password module to use bcrypt for password hashing and validation

---
+++ 0x00-personal_data/encrypt_password.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""encrypt logger module"""
+
+import bcrypt
+
+
+def hash_password(password: str) -> bytes:
+    """Hash a password using bcrypt, returning a salted, hashed password."""
+    salt = bcrypt.gensalt()
+    hashed_password = bcrypt.hashpw(password.encode("utf-8"), salt)
+    return hashed_password
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """Validate that the provided password matches the hashed password."""
+    return bcrypt.checkpw(password.encode("utf-8"), hashed_password)
