b398072099af64dd734477eeb48aaa18676fd7b5
piccolojnr <rahimdaud246@gmail.com>
2024-05-30 23:18:32 +0000

    feat: Add personal data module and logger
    The code changes include adding a new module for handling personal data and a logger module. The personal data module includes a Python script and an SQL file for setting up a MySQL server and creating a table for storing user data. The logger module includes a redacting formatter class and functions for filtering and obfuscating sensitive data. The main script retrieves user data from the database and logs it using the logger module.

---
+++ 0x00-personal_data/filtered_logger.py
@@ -0,0 +1,97 @@
+#!/usr/bin/env python3
+"""Filtered logger module"""
+
+import re
+import logging
+import os
+import mysql.connector
+from mysql.connector.connection import MySQLConnection
+
+
+PII_FIELDS = ("name", "email", "phone", "ssn", "password")
+
+
+class RedactingFormatter(logging.Formatter):
+    """Redacting Formatter class"""
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields):
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        original_message = super(RedactingFormatter, self).format(record)
+        return filter_datum(
+            self.fields,
+            self.REDACTION,
+            original_message,
+            self.SEPARATOR,
+        )
+
+
+def filter_datum(fields, redaction, message, separator) -> str:
+    """Returns the log message obfuscated"""
+    ss = "|".join(re.escape(f) for f in fields)
+    pattern = rf"({ss})=.*?(?={separator}|$)"
+    return re.sub(pattern, lambda m: f"{m.group(1)}={redaction}", message)
+
+
+def get_logger() -> logging.Logger:
+    """
+    Returns a logging.Logger object
+    """
+    logger = logging.Logger(name="user_data")
+    logger.level = logging.INFO
+    logger.propagate = False
+
+    stream_handler = logging.StreamHandler()
+    formatter = RedactingFormatter(fields=PII_FIELDS)
+    stream_handler.setFormatter(formatter)
+
+    logger.addHandler(stream_handler)
+    return logger
+
+
+def get_db() -> MySQLConnection:
+    """Get a database connection using credentials
+    from environment variables.
+    """
+    username = os.getenv("PERSONAL_DATA_DB_USERNAME", "root")
+    password = os.getenv("PERSONAL_DATA_DB_PASSWORD", "")
+    host = os.getenv("PERSONAL_DATA_DB_HOST", "localhost")
+    database = os.getenv("PERSONAL_DATA_DB_NAME")
+
+    if not database:
+        raise ValueError(
+            "The database name must be set in\
+                the environment variable PERSONAL_DATA_DB_NAME"
+        )
+
+    return mysql.connector.connect(
+        user=username,
+        password=password,
+        host=host,
+        database=database,
+    )
+
+
+def main():
+    """Main function"""
+    logger = get_logger()
+
+    db = get_db()
+    cursor = db.cursor(dictionary=True)
+    cursor.execute("SELECT * FROM users;")
+    for row in cursor:
+        message = "; ".join(f"{key}={value}" for key, value in row.items())
+        logger.info(message)
+
+    cursor.close()
+    db.close()
+
+
+if __name__ == "__main__":
+    main()
