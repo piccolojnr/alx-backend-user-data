911f93ad45bf2c0226d8ae818724eeca506f5d50
piccolojnr <rahimdaud246@gmail.com>
2024-06-04 22:54:59 +0000

    Refactor Auth class to include docstring and improve code readability

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -7,7 +7,9 @@ from typing import List, TypeVar
 
 
 class Auth:
-    """"""
+    """
+    Auth class
+    """
 
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """Require authentication"""
