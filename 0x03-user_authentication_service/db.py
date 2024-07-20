7150bb83202550700464de92a9ad0504ba88c2d7
piccolojnr <rahimdaud246@gmail.com>
2024-07-20 17:38:21 +0000

    Refactor DB class to improve error handling and query efficiency

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -1,37 +1,40 @@
 #!/usr/bin/env python3
-"""DB module
-In this task, you will complete the DB class
-provided below to implement the add_user method.
+"""DB module.
 """
 from sqlalchemy import create_engine, tuple_
+from sqlalchemy.exc import InvalidRequestError
+from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm.exc import NoResultFound
 from sqlalchemy.orm.session import Session
-from sqlalchemy.exc import NoResultFound, InvalidRequestError
-
 
 from user import Base, User
 
 
 class DB:
-    """DB class"""
+    """DB class.
+    """
 
     def __init__(self) -> None:
-        """Initialize a new DB instance"""
-        self._engine = create_engine("sqlite:///a.db", echo=True)
+        """Initialize a new DB instance.
+        """
+        self._engine = create_engine("sqlite:///a.db", echo=False)
         Base.metadata.drop_all(self._engine)
         Base.metadata.create_all(self._engine)
         self.__session = None
 
     @property
     def _session(self) -> Session:
-        """Memoized session object"""
+        """Memoized session object.
+        """
         if self.__session is None:
             DBSession = sessionmaker(bind=self._engine)
             self.__session = DBSession()
         return self.__session
 
     def add_user(self, email: str, hashed_password: str) -> User:
-        """Add a user to the database"""
+        """Adds
+        """
         try:
             new_user = User(email=email, hashed_password=hashed_password)
             self._session.add(new_user)
@@ -42,7 +45,8 @@ class DB:
         return new_user
 
     def find_user_by(self, **kwargs) -> User:
-        """Find a user by the given arguments"""
+        """Finds
+        """
         fields, values = [], []
         for key, value in kwargs.items():
             if hasattr(User, key):
@@ -57,8 +61,9 @@ class DB:
             raise NoResultFound()
         return result
 
-    def update_user(self, user_id: int, **kwargs) -> User:
-        """Update a user by the given arguments"""
+    def update_user(self, user_id: int, **kwargs) -> None:
+        """Update.
+        """
         user = self.find_user_by(id=user_id)
         if user is None:
             return
